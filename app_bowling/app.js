561e7047484d166cbf49fb70d2da88563370832a
Valentin <114942401+FrebourgValentin@users.noreply.github.com>
2022-12-02 14:45:59 +0100

    Add files via upload

--- app_bowling/app.js
+++ app_bowling/app.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-let rl = require('readline-sync');                                                              // Permet d'obtenir un flux d'entree/sortie via la console
+let readline = require('readline-sync');                                                        // Permet d'obtenir un flux d'entree/sortie via la console
 let score_l = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];                  // 21 lancers
 let score_m = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];                                                   // 10 manches
 let i = 0;                                                                                      // Numero de lancer
-let lancer_max = 2;                                                                             // Lancer max pour dernière manche
+let lancer_max = 2;                                                                             // Lancer max
 let s = ["|   ", "|   ", "|   ", "|   ", "|   ", "|   ", "|   ", "|   ", "|   ", "|   "];       // Ajuste les espaces entre les scores de chaque manche
 
 function tableau() {                                                                            // Fonction pour l'affichage du tableau dans la console
@@ -17,20 +17,20 @@ function tableau() {                                                            
 }
 
 for (let manche = 0; manche < 10; manche++) {                                                   // Boucle pour chaque manche
-    if (manche == 9) {                                                                          // A la dernière manche, le lancer max est de 3 (normalement en cas de spare ou de strike)
+    if (manche == 9) {                                                                          // Si on est a la derniere manche, le lancer max est de 3
         lancer_max = 3;
     }
 
     for (let lancer = 0; lancer < lancer_max; lancer++) {                                       // Boucle pour chaque lancer
-        console.log("Manche: " + (manche + 1) + " Lancer: " + (lancer + 1));
-        score_l[i] = rl.question("Quilles abattues entre 0 et 10: ");                           // L'utilisateur entre le nb de quille abattue dans la console
+        console.log("Manche: " + (manche + 1) + " Lancer: " + (lancer + 1));                    // Indique a l'utilisateur, la manche et le lancer actuel
+        score_l[i] = readline.question("Quilles abattues entre 0 et 10: ");                     // L'utilisateur entre le nb de quille abattue dans la console
 
         if ((score_l[i] >= 0 && score_l[i] <= 10 && lancer == 0 && manche < 9)                  // Si le nb de quille est compris entre 0 et 10 au premier lancer de chaque manche (sauf la derniere)
         || (score_l[i] >= 0 && score_l[i] <= 10 && lancer < 3 && manche == 9)                   // OU Si le nb de quille est compris entre 0 et 10 a tous les lancers de la derniere manche
         || ((Number(score_l[i - 1]) + Number(score_l[i])) <= 10 && lancer > 0)) {               // OU Si le nb de quille est compris entre 0 et 10 au second et troisieme lancer
 
             if (score_l[i] == 10 && lancer == 0) {                                              // Si le score est de 10 au premier lancer sur toutes les manches
-                if (manche < 9) { 
+                if (manche < 9) {                                                               // Si avant la derniere manche
                     score_l[i] = " ";
                     score_l[i + 1] = "X";
                     score_m[manche] += 10;
@@ -50,10 +50,10 @@ for (let manche = 0; manche < 10; manche++) {                                   
                         score_m[manche - 1] += 10;
                     }
 
-                    i++;
-                    lancer++;
+                    i++;                                                                        // Incremente le numero du lancer
+                    lancer++;                                                                   // Incremente le lancer
                 }
-                else if (manche == 9 && lancer == 0){
+                else if (manche == 9 && lancer == 0) {                                          // Si derniere manche et au premier lancer
                     score_l[i] = "X";
                     score_m[manche] += 10;
 
@@ -71,7 +71,7 @@ for (let manche = 0; manche < 10; manche++) {                                   
                         score_m[manche - 1] += 10;
                     }
                 }
-                else if (manche == 9 && lancer == 1){
+                else if (manche == 9 && lancer == 1) {                                          // Si derniere manche et au second lancer
                     score_l[i] = "X";
                     score_m[manche] += 10;
 
@@ -80,7 +80,7 @@ for (let manche = 0; manche < 10; manche++) {                                   
                         score_m[manche - 2] += 10;
                     } 
                 }
-                else if (manche == 9 && lancer == 2){
+                else if (manche == 9 && lancer == 2) {                                          // Si derniere manche et au troisieme lancer
                     score_l[i] = "X";
                     score_m[manche] += 10;
                 }
@@ -111,11 +111,11 @@ for (let manche = 0; manche < 10; manche++) {                                   
                 score_m[manche] += Number(score_l[i]) * 2;
                 score_m[manche - 1] += Number(score_l[i]);
             }
-            else if (score_l[i - 2] == "X" && manche < 9) {                                     // Si Strike il y a 2 lancer precedemment
+            else if (score_l[i - 2] == "X" && manche < 9) {                                     // Si Strike il y a 2 lancer precedemment avant la deniere manche
                 score_m[manche] += Number(score_l[i]) * 2;
                 score_m[manche - 1] += Number(score_l[i]);
             }
-            else if (score_l[i - 1] == "X" && manche == 9) {                                     // Si Strike il y a 2 lancer precedemment
+            else if (score_l[i - 1] == "X" && manche == 9) {                                    // Si Strike il y a 2 lancer precedemment pendant la derniere manche
                 score_m[manche - 1] += Number(score_l[i]);
             }
             else if (score_l[i] == 10 && manche == 9) {                                         // Si le score est de 10 a la derniere manche
@@ -133,40 +133,30 @@ for (let manche = 0; manche < 10; manche++) {                                   
             tableau();                                                                          // Affiche le tableau de la fonction dans la console
         }
         else {
-            console.log("Erreur, reessayez: ");
-            lancer--;
+            console.log("Erreur, reessayez: ");                                                 // Indique a l'utilisateur une erreur sur nb de quilles abattues
+            lancer--;                                                                           // Le lancer actuel ne compte pas
         }
     }
 
     score_m[manche + 1] += score_m[manche];                                                     // Cumule les points a chaque manche
 
-    if (score_m[manche] <= 9) {
+    if (score_m[manche] <= 9) {                                                                 // Si le score de la manche contient un chiffre
         s[manche] = "|   ";
     } 
-    else if (score_m[manche] >= 10 && score_m[manche] <= 99) {
+    else if (score_m[manche] >= 10 && score_m[manche] <= 99) {                                  // Si le score de la manche contient 2 chiffres
         s[manche] = "|  ";
     }
-    else if (score_m[manche] >= 100 && score_m[manche] <= 999) {
+    else if (score_m[manche] >= 100 && score_m[manche] <= 999) {                                // Si le score de la manche contient 3 chiffres
         s[manche] = "| ";
     }
     
-    if (score_m[manche - 1] <= 9) {
+    if (score_m[manche - 1] <= 9) {                                                             // Si le score de la manche precedente contient un chiffre
         s[manche - 1] = "|   ";
     } 
-    else if (score_m[manche - 1] >= 10 && score_m[manche - 1] <= 99) {
+    else if (score_m[manche - 1] >= 10 && score_m[manche - 1] <= 99) {                          // Si le score de la manche precedente contient 2 chiffres
         s[manche - 1] = "|  ";
     }
-    else if (score_m[manche - 1] >= 100 && score_m[manche - 1] <= 999) {
+    else if (score_m[manche - 1] >= 100 && score_m[manche - 1] <= 999) {                        // Si le score de la manche precedente contient 3 chiffres
         s[manche - 1] = "| ";
-    }
-
-    if (score_m[manche - 2] <= 9) {
-        s[manche - 2] = "|   ";
-    } 
-    else if (score_m[manche - 2] >= 10 && score_m[manche - 2] <= 99) {
-        s[manche - 2] = "|  ";
-    }
-    else if (score_m[manche - 2] >= 100 && score_m[manche - 2] <= 999) {
-        s[manche - 2] = "| ";
     }
 }
